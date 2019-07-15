f492b9597bbf57a6112b3b3bc4f1b185edb48d96
raouia bouabdallah <raouia.bouabdellah@gmail.com>
2019-07-15 02:06:41 +0100

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about me</h1>
+    <p>I am a software engineer and currently a phd student from the higher institute of management University of tunis and a member of the COSMOS research laboratory (ENSI). My research interest involves optimization of resources allocation in the cloud federation, scaling cloud applications and performance in the cloud. I am also a teacher at the Sfax university for more than 4 years. I am as well into a lot of different things like graphical designing and software programming (Node.js , React.js, React Native) . I am keen on computer science. </p>
+  </div>
+</template>
